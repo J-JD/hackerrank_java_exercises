f5a3c2eea21e6eef9cbc7990c73167a8fb03358d
John Doe <jamesjohndoe@protonmail.com>
2021-10-02 14:25:48 +0200

    feat: Add Node class for exercise day 22

--- src/App.java
+++ src/App.java
@@ -12,5 +12,14 @@ public class App {
 		BinaryNumbers binaryNumbers = new BinaryNumbers();
 		System.out.println("Day 10. Binary Numbers solution is: " + binaryNumbers.solution(951));
 
+		// 30 Days of Code in Java Day 22. Binary Search Trees
+		// Variables:
+		int[] arrayBTS = { 3, 5, 2, 1, 4, 6, 7 };
+		Node root = null;
+		for (int i = 0; i < arrayBTS.length; i++) {
+			root = Node.insert(root, arrayBTS[i]);
+		}
+		System.out.println("L22 NODE: " + root);
+
 	} // END main
 } // END class App
---
+++ src/Node.java
@@ -0,0 +1,54 @@
+public class Node {
+    // Member fields
+    private Node left;
+    private Node right;
+    private int data;
+
+    // Constructor
+    public Node(int data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    // Method
+    public static Node insert(Node root, int data) {
+        if (root == null) {
+            return new Node(data);
+        } else {
+            Node cur;
+            if (data <= root.data) {
+                cur = insert(root.left, data);
+                root.left = cur;
+            } else {
+                cur = insert(root.right, data);
+                root.right = cur;
+            }
+            return root;
+        }
+    }
+} // END class NodeBST
